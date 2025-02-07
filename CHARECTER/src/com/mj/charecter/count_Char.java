f68f99e047f2275832ff243c2ecf29581a13aefc
ANAND <ANAND@14-dv0543TU>
2025-02-07 10:09:43 +0530

    EX CHARECTER PROGRAM

---
+++ CHARECTER/src/com/mj/charecter/count_Char.java
@@ -0,0 +1,15 @@
+package com.mj.charecter;
+
+
+
+public class count_Char {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println('j'+'a'+'v'+'a');
+
+	}
+
+}
---
+++ CHARECTER/src/com/mj/test/test.java
@@ -0,0 +1,19 @@
+package com.mj.test;
+
+public class test {
+
+	
+	public static void main(String[] args) 
+	{
+//		if(2==2)
+//		{
+//			break;
+//		}
+//		else {
+//			System.out.println("Hello");
+//		}
+//		System.out.println("bitLables");
+
+	}
+
+}
